5984e08e1e4154850cf2eb670a7a2a19174b13b1
Nakul Thakur <thakurnakul119@gmail.com>
2024-01-20 00:37:53 +0530

    changed purchased model

--- model/PurchasedCourseModel.js
+++ model/PurchasedCourseModel.js
@@ -1,14 +1,18 @@
 const mongoose=require('mongoose')
 const PurchasedCourseSchema=new mongoose.Schema({
     user_id:{type:String,required:true},
+    course_id:{type:String,required:true},
     course_name:{type:String,required:true},
     course_type:{type:String,required:true},
     status:{type:String,required:true},
-    preferred_timing:{type:String,requires:true},
-    duration:{type:String,requires:true},
+    preferred_timing:{type:String,required:true},
+    duration:{type:String,required:true},
     starting_date:{type:String,required:true},
     expiration_date:{type:String,required:true},
     price:{type:Number,required:true},
-    date:{type:Date,default:Date.now()}
+    date:{type:Date,default:Date.now()},
+    days:[{ type: String }],
+    transaction_id:{type:String,required:true},
+    transaction_status:{type:String,required:true},
 })
 module.exports=mongoose.model("Purchase_Course",PurchasedCourseSchema)
