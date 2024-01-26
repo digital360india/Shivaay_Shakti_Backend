32aa83b5cae49695536d5602a87225a54c7eaabe
Nakul Thakur <thakurnakul119@gmail.com>
2024-01-27 01:12:52 +0530

    fix in controller

--- controllers/PurchasedCourse/UpdateGroupLinkController.js
+++ controllers/PurchasedCourse/UpdateGroupLinkController.js
@@ -1,14 +1,17 @@
 const Purchased=require('../../model/PurchasedCourseModel')
 const UpdateGroupLinkController=async(req,res)=>{
     const { course_name, preferred_timing, link } = req.body;
+    console.log(req.body)
     try{
-        await Course.updateMany(
+        const response=await Purchased.updateMany(
             { course_name, preferred_timing },
             { $set: { link: link } }
           );
+          console.log(response)
     res.status(200).json({message:"updated successful"})
     }
     catch(error){
+        console.log(error)
             res.status(500).json({ message: "Server Error" });
         
     }
