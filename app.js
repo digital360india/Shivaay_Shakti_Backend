eaf6b70991353cb5633d0d59ae08bc1137696606
Himanshika Rawat <himanshirawat604@gmail.com>
2024-07-26 11:15:04 +0530

    new updates

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const ArticleRouter=require('./routes/ArticleRouter')
 const PurchasedCourseRouter=require('./routes/PurchasedCourseRouter')
 const cors=require('cors')
 const app=express()
-const port=process.env.PORT ||5000;
+const port= process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
 app.get('/',(req,res)=>{
