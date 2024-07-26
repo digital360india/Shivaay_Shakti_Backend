840b011b92fa10d6d7e545b4995759230ff9fa80
Himanshika Rawat <himanshirawat604@gmail.com>
2024-07-26 11:00:49 +0530

    ready for deployment

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const ArticleRouter=require('./routes/ArticleRouter')
 const PurchasedCourseRouter=require('./routes/PurchasedCourseRouter')
 const cors=require('cors')
 const app=express()
-const port=process.env.PORT ||5001;
+const port=process.env.PORT ||5000;
 app.use(cors())
 app.use(express.json())
 app.get('/',(req,res)=>{
@@ -19,6 +19,6 @@ app.use('/api/course',CourseRouter);
 app.use('/api/survey',SurveyRouter);
 app.use('/api/article',ArticleRouter);
 app.use('/api/purchase',PurchasedCourseRouter);
-app.listen(5000,()=>{
-    `listening to port ${port}`;
-})
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+  });
