fe4137b604005f5259927c70fd024d0f844f9280
Himanshika Rawat <himanshirawat604@gmail.com>
2024-09-24 13:03:18 +0530

    links added

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin: ["https://www.shivaayshaktiyog.com/"], 
+  origin: ["https://www.shivaayshaktiyog.com"], 
 }));
 
 app.get("/", (req, res) => {
