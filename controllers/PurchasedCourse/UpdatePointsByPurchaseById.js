c09bbc32e1b7680a558ec9179f50bd73de66b8c7
digital360india <139943711+digital360india@users.noreply.github.com>
2024-11-27 16:31:19 +0530

    bh

--- controllers/PurchasedCourse/UpdatePointsByPurchaseById.js
+++ controllers/PurchasedCourse/UpdatePointsByPurchaseById.js
@@ -3,27 +3,27 @@ const Purchased = require('../../model/PurchasedCourseModel');
 const UpdatePointsByPurchaseById = async (req, res) => {
   console.log(req.params);
   console.log(req.body);
-  const { _id } = req.params;
+  // const { _id } = req.params;
   try {
-    const response = await Purchased.findById(_id);
-    if(!response.points)
-    {
-     response.points=[];
-     response?.points?.push(req.body);
-     await response.save();
-     res.status(200).json({ message: "Updated successfully", data: response });
-    }
+    // const response = await Purchased.findById(_id);
+    // if(!response.points)
+    // {
+    //  response.points=[];
+    //  response?.points?.push(req.body);
+    //  await response.save();
+    //  res.status(200).json({ message: "Updated successfully", data: response });
+    // }
     
-    if(response?.points?.find(a=>a?.date1===req.body?.date1))
-    {
-      res.status(400).json({ message: "Data already submitted", data: response });
+    // if(response?.points?.find(a=>a?.date1===req.body?.date1))
+    // {
+    //   res.status(400).json({ message: "Data already submitted", data: response });
       
-    }
-    else{
-      response?.points?.push(req.body);
-      await response.save();
-      res.status(200).json({ message: "Updated successfully", data: response });
-    }
+    // }
+    // else{
+    //   response?.points?.push(req.body);
+    //   await response.save();
+    //   res.status(200).json({ message: "Updated successfully", data: response });
+    // }
     } 
    catch (error) {
     console.error(error);
